eec2132943ace5e85b150864315d4737b3b05ef9
RodrigoGax <36560095+RodrigoGax@users.noreply.github.com>
2023-03-06 12:41:14 -0600

    Initial Commit

---
+++ GameStreamRG/GameStreamRGApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamRGApp.swift
+//  GameStreamRG
+//
+//  Created by Rodrigo Fidel Gaxiola Sosa on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamRGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
